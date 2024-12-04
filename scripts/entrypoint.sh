f774bd1381183c0d09d2d4b5ba551283566b5f79
Wessel <wesselvdp@gmail.com>
2024-12-04 17:10:33 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,7 @@
 
 # Run migrations
 migrate -database "sqlite3://${DB_PATH}?_foreign_keys=on"  -verbose -path /db/migrations up
+
 #migrate -database "sqlite3://database.db?_foreign_keys=on"  -verbose -path db/migrations up
 
 
